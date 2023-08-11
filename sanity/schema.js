f23be252625849a7888f0728a23b83f8cfb607d2
baggiiiie <yingchao.dai22@gmail.com>
2023-08-11 22:16:58 +0800

    added sanity

---
+++ sanity/schema.js
@@ -0,0 +1,10 @@
+import experience from './schemas/experience';
+import pageInfo from './schemas/pageInfo';
+import project from './schemas/project';
+import social from './schemas/social';
+
+export const schema = {
+	types: [
+		pageInfo, project, experience, social,
+	],
+};
