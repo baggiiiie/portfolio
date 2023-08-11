a3ff79df1c78597f5ab41059793f5c2f30e5067d
baggiiiie <yingchao.dai22@gmail.com>
2023-08-11 21:28:28 +0800

    re-install sanity

--- sanity/schema.js
+++
@@ -1,8 +0,0 @@
-import blockContent from './schemas/blockContent';
-import category from './schemas/category';
-import pageInfo from './schemas/post';
-import author from './schemas/author';
-
-export const schema = {
-  types: [pageInfo, author, category, blockContent],
-};
