2f779bf4ef630be3b845986e5b91189a7b531207
baggiiiie <yingchao.dai22@gmail.com>
2023-09-02 00:07:12 +0800

    updated experience card and project card css

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,7 +29,8 @@ function Projects({ projects }: Props) {
 							onClick={() => content === null && setContent(project)}
 							key={project._id}
 							className="projectCoverContainer"
-							layoutId={`project-card-img-container-${project._id}`}
+							layoutId={`project-card-container-${project._id}`}
+							// layoutId={`project-card-img-container-${project._id}`}
 						>
 							<img
 								src={`${project.logo}`}
@@ -39,16 +40,45 @@ function Projects({ projects }: Props) {
 						</motion.div>
 					))}
 				</div>
-				<p className="text-gray-500 hover:text-gray-300 transition-all mt-10 text-center font-light text-sm">
+				<p className="text-gray-500 hover:text-gray-300 transition-all duration-300 mt-10 text-center font-light text-sm">
 					The card opening effect is achieved with framer motion, double opening
 					a card results in a weird zooming behavior, which hasn't been resolved
 					yet but i really wanna use this effect :(
 				</p>
 				<AnimatePresence>
 					{content && (
-						<motion.div onClick={handleClose} className="projectCardContainer">
-							<ProjectCard project={content} />
-						</motion.div>
+						<>
+							<motion.div
+								onClick={handleClose}
+								layoutId={`project-card-container-${content._id}`}
+								className="projectCardContainer"
+								initial={{ opacity: 0 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+							>
+								<ProjectCard project={content} />
+							</motion.div>
+							{/* <motion.div
+								variants={{
+									hidden: {
+										opacity: 0,
+										transition: {
+											duration: 0.16,
+										},
+									},
+									visible: {
+										opacity: 0.8,
+										transition: {
+											delay: 0.04,
+											duration: 0.2,
+										},
+									},
+								}}
+								initial="hidden"
+								exit="hidden"
+								animate="visible"
+							/> */}
+						</>
 					)}
 				</AnimatePresence>
 			</div>
