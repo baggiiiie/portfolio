be97126a944d2b44e58d05b6eab4d3a0ba3756b8
baggiiiie <yingchao.dai22@gmail.com>
2023-09-03 18:20:49 +0800

    added project redirection

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	rewrites() {
+		return {
+			beforeFiles: [
+				// if the host is `app.acme.com`,
+				// this rewrite will be applied
+				{
+					source: "/:path*",
+					has: [
+						{
+							type: "host",
+							value: "netflix.yingchaoooo.me",
+						},
+					],
+					destination: "/netflix/:path*",
+				},
+			],
+		};
+	},
+};
 
 module.exports = nextConfig;
-// module.exports = {
-//     async rewrites() {
-//         return [
-//             {
-//                 source: '/netfilx',
-//                 destination: 'https://yingchaoooo.me',
-//             },
-//         ];
-//     },
-// };
