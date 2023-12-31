8bf638e5932147e7c1d97f965b6ea2a310a7fde4
baggiiiie <yingchao.dai22@gmail.com>
2023-12-31 11:56:04 +0800

    add blog page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const BlogPage: React.FC = () => {
+	return (
+		<div
+			className="bg-white
+			text-black
+			h-[100vh]
+			text-center
+		"
+		>
+			<h1>Blog Page</h1>
+			{/* Add your blog content here */}
+			<p>This is a test</p>
+		</div>
+	);
+};
+
+export default BlogPage;
