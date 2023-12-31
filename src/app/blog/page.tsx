18a166cbe9bceb8664538e99764441b01938d9e2
baggiiiie <yingchao.dai22@gmail.com>
2023-12-31 14:11:15 +0800

    set up blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,17 +1,38 @@
+"use client";
 import React from "react";
-
 const BlogPage: React.FC = () => {
 	return (
+		// page div
 		<div
 			className="bg-white
 			text-black
-			h-[100vh]
 			text-center
 		"
 		>
-			<h1>Blog Page</h1>
-			{/* Add your blog content here */}
-			<p>This is a test</p>
+			{/* display sheet div */}
+			<div
+				className="w-[70%] max-w-[800px]
+				h-[100vh] py-10
+			bg-gray-100 m-auto"
+			>
+				{/* header div */}
+				<div className="py-5">
+					<h1 className="font-semibold text-2xl pb-2">
+						My thoughts and gibberish
+					</h1>
+					<h2 className="text-sm italic">This is your hackernews style blog</h2>
+				</div>
+				{/* content div */}
+				<div className="px-20">
+					{/* individual blog div */}
+					<a href="/blog/blog-1">
+						<div className="flex justify-between hover:underline hover:bg-blue-100 px-4 hover:cursor-pointer">
+							<p className="">This is a test for blog title</p>
+							<p>2023/12/31</p>
+						</div>
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 };
