fdd3c180ac7a0bba11b73fedd44a36e7f57935cc
yc <yingchao.dai@bytedance.com>
2024-07-05 21:59:51 +0800

    Better redirects

--- next.config.js
+++ next.config.js
@@ -1,32 +1,36 @@
 /** @type {import('next').NextConfig} */
+
+const myDomain = "yingchaoooo.com";
+
 const nextConfig = {
-	rewrites() {
-		return {
-			beforeFiles: [
-				// if the host is `app.acme.com`,
-				// this rewrite will be applied
-				{
-					source: "/:path*",
-					has: [
-						{
-							type: "host",
-							value: "project1.yingchaoooo.me",
-						},
-					],
-					destination: "/project1/:path*",
-				},
-			],
-		};
-	},
-	async redirects() {
-		return [
-		  {
-			source: '/blog',
-			destination: 'https://blog.yingchaoooo.me',
-			permanent: false
-		  }
-		];
-	},
+  rewrites() {
+    return {
+      beforeFiles: [
+        // if the host is `app.acme.com`,
+        // this rewrite will be applied
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: `project1.${myDomain}`,
+            },
+          ],
+          destination: "/project1/:path*",
+        },
+      ],
+    };
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/blog",
+        destination: `https://blog.${myDomain}`,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
